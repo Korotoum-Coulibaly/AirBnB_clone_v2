dfcc1176a916f8dc4ee825a53b241d5d1094cc43
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-04-19 20:32:03 -0700

    add delete method and udpate all method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,18 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """
+        Returns a dictionary which contains all objects
+        of a specified class or all classes if no type is specified.
+        """
+        if cls is not None:
+            filtered_dict = {}
+            for k, v in self.__objects.items():
+                if cls == v.__class__ or cls == v.__class__.__name__:
+                    filtered_dict[k] = v
+            return filtered_dict
+        self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +33,13 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """Deletes obj form __objects"""
+        if obj is not None:
+            key = type(obj).__name + '.' + obj.id
+            if key in self.__objects:
+                del sel.__objects[key]
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -45,6 +61,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
