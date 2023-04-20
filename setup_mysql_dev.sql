f2218aaf544c57016065913c6d034ba49836c68f
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2023-04-19 18:58:11 -0700

    mysql initialize

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,10 @@
 -- Mysql configuration
-
-HBNB_ENV = dev
-HBNB_MYSQL_USER = hbnb_dev
-HBNB_MYSQL_PWD = hbnb_dev_pwd
-HBNB_MYSQL_HOST = localhost
-HBNB_MYSQL_DB = hbnb_dev_db
-HBNB_TYPE_STORAGE = DBStorage
+import mysql.connector
+connection = mysql.connector.connect(
+		HBNB_ENV = "dev",
+		HBNB_MYSQL_USER = "hbnb_dev",
+		HBNB_MYSQL_PWD = "hbnb_dev_pwd",
+		HBNB_MYSQL_HOST = "localhost",
+		HBNB_MYSQL_DB = "hbnb_dev_db",
+		HBNB_TYPE_STORAGE = "DBStorage")
+cursor = connection.cursor(prepared = true)
