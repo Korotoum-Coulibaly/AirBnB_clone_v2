c6e098e41c2307f79ba8187504b572f9cbc15789
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-04-19 19:41:44 -0700

    MySQL server prepared

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,13 +1,6 @@
--- Mysql configuration
-import mysql.connector
-connection = mysql.connector.connect(
-		HBNB_ENV = "dev",
-		HBNB_MYSQL_USER = "hbnb_dev",
-		HBNB_MYSQL_PWD = "hbnb_dev_pwd",
-		HBNB_MYSQL_HOST = "localhost",
-		HBNB_MYSQL_DB = "hbnb_dev_db",
-		HBNB_TYPE_STORAGE = "DBStorage")
-cursor = connection.cursor(prepared = true)
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost'
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost'
-GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost
+-- Script that prepares MySQL server for the project
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
