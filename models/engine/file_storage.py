7728da86c90096190014c53d05b9da757c22566b
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-04-19 21:21:25 -0700

    file storage updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
+classes = {'BaseModel': BaseModel, 'User': User, 'Place': Place, 'State': State, 'City': City, 'Amenity': Amenity, 'Review': Review}
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
@@ -39,30 +47,16 @@ class FileStorage:
     def delete(self, obj=None):
         """Deletes obj form __objects"""
         if obj is not None:
-            key = type(obj).__name + '.' + obj.id
+            key = type(obj).__name__ + '.' + obj.id
             if key in self.__objects:
                 del self.__objects[key]
 
     def reload(self):
-        """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+        """deserializes the JSON file to __objects"""
         try:
-            temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+                    self.__objects[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
