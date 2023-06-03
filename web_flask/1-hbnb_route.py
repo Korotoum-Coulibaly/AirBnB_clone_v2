32345901f813c64eb0caf9d758946a060125bf8d
Esther Coulibaly <coulibalykorotoum0808@gmail.com>
2023-06-03 20:54:02 +0000

    task 1 web flask

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """ Function that  display hello on computer screen"""
+    return 'Hello HBNB!'
+
+@app.route('/hnbnb', strict_slashes=False)
+def HBNB():
+    """ Function that display hbnb on computer screen"""
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
