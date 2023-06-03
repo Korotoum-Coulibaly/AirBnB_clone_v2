9a58229e1cfb5947ad7ae7c9034086aeca539c32
Esther Coulibaly <coulibalykorotoum0808@gmail.com>
2023-06-03 18:54:13 +0000

    function documentation

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-""" Write a Fabric script that distributes an archive to my web servers,using the function do_deploy: """
-
 from fabric.api import *
 from datetime import datetime
 from os.path import exists
@@ -9,7 +7,8 @@ env.hosts = ['100.27.5.96', '100.25.180.2']
 #<IP web-01>', 'IP web-02'
 
 def do_deploy(archive_path):
-    """ Fabric script that distributes an archive to my web servers"""
+    """ Fabric script that distributes an archive
+    to my web servers"""
 
     if exists(archive_path) is false:
 	return False 
@@ -23,14 +22,14 @@ def do_deploy(archive_path):
 	#download the archive file to the tmp directory
 	run("mkdir -p {}/".format(tgz))
 	#dzip archive to the folder /data/web_static/releases/
-	run("tar -xzf {} -C {}/".format(tmp, no_tgz))
+	run("tar -xzf {} -C {}/".format(tmp, tgz))
 	run("rm {}".format(tmp))
-	run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
-	run("rm -rf {}/web_static".format(no_tgz))
+	run("mv {}/web_static/* {}/".format(tgz, tgz))
+	run("rm -rf {}/web_static".format(tgz))
 	#delete archive
 	run("rm -rf /data/web_static/current")
 	#delete symbolic link
-	run("ln -s {}/ /data/web_static/current".format(no_tgz))
+	run("ln -s {}/ /data/web_static/current".format(tgz))
 	#create a new symbolic linc
 	return True
     except:
