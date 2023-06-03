032f67c22d242ccda154065b48f6ff8666b542ab
Esther Coulibaly <coulibalykorotoum0808@gmail.com>
2023-06-03 21:19:32 +0000

    task 3 web flask

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,6 +22,7 @@ def C_by_value_text(text):
     text = text.replace("_", " ")
     return 'C {}'.format(text)
 
+
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def Python_is_fun(text="is_cool"):
