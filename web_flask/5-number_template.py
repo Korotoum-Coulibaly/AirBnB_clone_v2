8ca9a51d51361ade9b1cdfb38445a99a01ee9ec3
Esther Coulibaly <coulibalykorotoum0808@gmail.com>
2023-06-03 21:29:39 +0000

    task 5 web flask

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """ Function that  display hello on computer screen"""
+    return 'Hello HBNB!'
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """ Function that display hbnb on computer screen"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_by_value_text(text):
+    """ Function that returns C with the text"""
+    text = text.replace("_", " ")
+    return 'C {}'.format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def Python_is_fun(text="is_cool"):
+    """ Function that returns python on a womputer screen"""
+    text = text.replace("_", " ")
+    return 'Python {}'.format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """ Function that returns number on a computer screen"""
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """ Function that return number in template"""
+    return render_template('5-number.html', number=n)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
