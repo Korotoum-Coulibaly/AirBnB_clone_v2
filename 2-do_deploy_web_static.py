d9f59e7974b219fc093c2ad3e42edda5b3b1c382
Esther Coulibaly <coulibalykorotoum0808@gmail.com>
2023-06-03 19:06:36 +0000

    documentation

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,25 +11,19 @@ def do_deploy(archive_path):
 
     if exists(archive_path) is false:
 	return False 
-	#returns false if the file at archive_path doesnt exist
     filename = archive_path.split('/')[-1]
     tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
     tmp = "/tmp/" + filename
 
     try:
 	put(archive_path, "/tmp/")
-	#download the archive file to the tmp directory
 	run("mkdir -p {}/".format(tgz))
-	#dzip archive to the folder /data/web_static/releases/
 	run("tar -xzf {} -C {}/".format(tmp, tgz))
 	run("rm {}".format(tmp))
 	run("mv {}/web_static/* {}/".format(tgz, tgz))
 	run("rm -rf {}/web_static".format(tgz))
-	#delete archive
 	run("rm -rf /data/web_static/current")
-	#delete symbolic link
 	run("ln -s {}/ /data/web_static/current".format(tgz))
-	#create a new symbolic linc
 	return True
     except:
 	return False
